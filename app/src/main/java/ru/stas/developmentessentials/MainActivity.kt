ce9e154be65d72b90362814df3825fee61c29b8a
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-06 23:35:20 +0300

    setOnClick add

--- app/src/main/java/ru/stas/developmentessentials/MainActivity.kt
+++ app/src/main/java/ru/stas/developmentessentials/MainActivity.kt
@@ -1,21 +1,35 @@
 package ru.stas.developmentessentials
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import ru.stas.developmentessentials.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        convertCurrency()
     }
 
-    fun convertCurrency(view: View) = if(dollarText.text.isNotEmpty()){
-        val dollarValue = dollarText.text.toString().toFloat()
-        val euroValue = dollarValue * 0.85f
-        textView.text = "$euroValue euro"
-    }else{
-        textView.text = getString(R.string.no_value_string)
+    private fun convertCurrency(){
+        val button = binding.btnShow
+        val image = binding.ivHello
+        button.setOnClickListener {
+            image.isVisible = true
+        }
     }
 }
