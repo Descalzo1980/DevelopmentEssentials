680473f731cba966c439e0f49f6bad4eaf1dde6a
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-06 22:16:07 +0300

    Initial commit

---
+++ app/src/main/java/ru/stas/developmentessentials/MainActivity.kt
@@ -0,0 +1,21 @@
+package ru.stas.developmentessentials
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun convertCurrency(view: View) = if(dollarText.text.isNotEmpty()){
+        val dollarValue = dollarText.text.toString().toFloat()
+        val euroValue = dollarValue * 0.85f
+        textView.text = "$euroValue euro"
+    }else{
+        textView.text = getString(R.string.no_value_string)
+    }
+}
